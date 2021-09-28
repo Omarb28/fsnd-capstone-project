5a7f9c2fad73f3e303e0934f34ed172dc5d97db3
Omarb28 <omarb28@gmail.com>
2021-09-28 13:44:46 +0300

    Format code for PEP 8

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from models import setup_db
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
-  db = setup_db(app)
-  migrate = Migrate(app, db)
 
-  return app
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+    db = setup_db(app)
+    migrate = Migrate(app, db)
+
+    return app
+
 
 APP = create_app()
 
--- models.py
+++ models.py
@@ -5,11 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 database_path = os.environ['DATABASE_URL']
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app, database_path=database_path):
+    '''
+    setup_db(app)
+    Binds a flask application and a SQLAlchemy service
+    '''
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -20,50 +21,50 @@ def setup_db(app, database_path=database_path):
     return db
 
 
-'''
-Movie Model
-Has attributes title and release year
-'''
-class Movie(db.Model):  
-  __tablename__ = 'Movie'
+class Movie(db.Model):
+    '''
+    Movie Model
+    Has attributes title and release year
+    '''
+    __tablename__ = 'Movie'
 
-  id = Column(Integer, primary_key=True)
-  title = Column(String)
-  release_year = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_year = Column(Integer)
 
-  def __init__(self, title, release_year):
-    self.title = title
-    self.release_year = release_year
+    def __init__(self, title, release_year):
+        self.title = title
+        self.release_year = release_year
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_year': self.release_year
-    }
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_year': self.release_year
+        }
 
 
-'''
-Actor Model
-Has attributes  name, age and gender
-'''
 class Actor(db.Model):
-  __tablename__ = 'Actor'
+    '''
+    Actor Model
+    Has attributes  name, age and gender
+    '''
+    __tablename__ = 'Actor'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  age = Column(Integer)
-  gender = Column(String) # TODO: Restrict column to male and female
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)  # TODO: Restrict column to male and female
 
-  def __init__(self, name, age, gender):
-    self.name = name
-    self.age = age
-    self.gender = gender
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'age': self.age,
-      'gender': self.gender
-    }
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
