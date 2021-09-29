3e31df42a8d343f29ee643376945ec600686f4b0
Omarb28 <omarb28@gmail.com>
2021-09-30 01:37:41 +0300

    Add more unit tests for endpoints and RBAC roles

--- src/test_app.py
+++ src/test_app.py
@@ -8,7 +8,7 @@ from database.models import setup_db, Actor, Movie
 
 
 class CastingAgencyTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
+    """This class represents casting agency test cases"""
 
     def setUp(self):
         """Define test variables and initialize app."""
@@ -17,23 +17,62 @@ class CastingAgencyTestCase(unittest.TestCase):
         self.database_url = os.environ['DATABASE_URL']
         setup_db(self.app, self.database_url)
 
-        self.new_actor = {
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.drop_all()
+            self.db.create_all()
+
+            # initial seed data
+            self.actors_list = [
+                {'name': 'Leonardo DiCaprio', 'age': 46, 'gender': 'Male'},
+                {'name': 'Kate Winslet', 'age': 45, 'gender': 'Female'},
+                {'name': 'Cary Elwes', 'age': 58, 'gender': 'Male'}
+            ]
+
+            self.movies_list = [
+                {'title': 'Titanic', 'release_year': 1997},
+                {'title': 'SAW', 'release_year': 2004},
+                {'title': 'Get Out', 'release_year': 2017}
+            ]
+
+            for actor in self.actors_list:
+                actor_row = Actor(name=actor['name'], age=actor['age'], gender=actor['gender'])
+                actor_row.insert()
+
+            for movie in self.movies_list:
+                movie_row = Movie(title=movie['title'], release_year=movie['release_year'])
+                movie_row.insert()
+
+        # variables to use in the tests
+        self.actor_test = {
             'name': 'Will Smith',
             'age': 53,
             'gender': 'Male'
         }
 
-        self.new_movie = {
+        self.movie_test = {
             'title': 'Bad Boys for Life',
             'release_year': 2020
         }
 
-        self.new_actor_missing_fields = {
+        self.actor_with_missing_attributes = {
             'name': 'Dwayne Johnson',
         }
 
-        self.new_movie_missing_fields = {
+        self.movie_with_missing_attributes = {
             'title': 'Jumanji: The Next Level'
+        }
+
+        self.actor_with_wrong_attributes = {
+            'name': 'Jackie Chan',
+            'age': -67
+        }
+
+        self.movie_with_wrong_attributes = {
+            'title': 'Rush Hour',
+            'release_year': -1998
         }
 
         self.token_auth = {
@@ -43,12 +82,6 @@ class CastingAgencyTestCase(unittest.TestCase):
         }
 
         self.default_token_auth = self.token_auth['executive_producer_auth']
-
-        # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            self.db.create_all()
 
     def tearDown(self):
         """Executed after each test"""
@@ -82,7 +115,7 @@ class CastingAgencyTestCase(unittest.TestCase):
     
     def test_404_actor_not_found(self):
         res = self.client().get(
-            '/actors/9999', 
+            '/actors/99999', 
             headers=self.default_token_auth)
         data = json.loads(res.data.decode('utf-8'))
 
@@ -95,7 +128,7 @@ class CastingAgencyTestCase(unittest.TestCase):
     def test_create_new_actor(self):
         res = self.client().post(
             '/actors',
-            json=self.new_actor, 
+            json=self.actor_test, 
             headers=self.default_token_auth)
         data = json.loads(res.data.decode('utf-8'))
 
@@ -103,16 +136,250 @@ class CastingAgencyTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['created_id'])
     
-    def test_422_create_new_actor_with_missing_fields(self):
+    def test_400_bad_request_create_new_actor_with_missing_attributes(self):
         res = self.client().post(
             '/actors',
-            json=self.new_actor_missing_fields, 
+            json=self.actor_with_missing_attributes, 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Bad Request')
+    
+    # Patch Actor
+
+    def test_update_actor_information(self):
+        res = self.client().patch(
+            '/actors/1',
+            json=self.actor_test, 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['actor'])
+        self.assertEqual(data['actor']['name'], self.actor_test['name'])
+    
+    def test_422_unprocessable_update_actor_with_wrong_attributes(self):
+        res = self.client().patch(
+            '/actors/1',
+            json=self.actor_with_wrong_attributes, 
             headers=self.default_token_auth)
         data = json.loads(res.data.decode('utf-8'))
 
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Unprocessable Entity')
+
+    # Delete Actor
+
+    def test_delete_actor(self):
+        res = self.client().delete(
+            '/actors/2',
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['deleted_id'])
+    
+    def test_404_not_found_actor_to_delete(self):
+        res = self.client().delete(
+            '/actors/99999',
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    '''
+    Movie Endpoint Tests
+    '''
+
+    # Get Movie
+
+    def test_get_list_of_movies(self):
+        res = self.client().get(
+            '/movies', 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['movies']))
+
+    def test_get_movie_information(self):
+        res = self.client().get(
+            '/movies/1', 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['movie'])
+    
+    def test_404_movie_not_found(self):
+        res = self.client().get(
+            '/movies/99999', 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+    
+    # Post Movie
+
+    def test_create_new_movie(self):
+        res = self.client().post(
+            '/movies',
+            json=self.movie_test, 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created_id'])
+    
+    def test_400_bad_request_create_new_movie_with_missing_attributes(self):
+        res = self.client().post(
+            '/movies',
+            json=self.movie_with_missing_attributes, 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Bad Request')
+    
+    # Patch Movie
+
+    def test_update_movie_information(self):
+        res = self.client().patch(
+            '/movies/1',
+            json=self.movie_test, 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['movie'])
+        self.assertEqual(data['movie']['title'], self.movie_test['title'])
+    
+    def test_422_unprocessable_update_movie_with_wrong_attributes(self):
+        res = self.client().patch(
+            '/movies/1',
+            json=self.movie_with_wrong_attributes, 
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable Entity')
+
+    # Delete Movie
+
+    def test_delete_movie(self):
+        res = self.client().delete(
+            '/movies/2',
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['deleted_id'])
+    
+    def test_404_not_found_movie_to_delete(self):
+        res = self.client().delete(
+            '/movies/99999',
+            headers=self.default_token_auth)
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    '''
+    Role RBAC Tests
+    '''
+
+    # Casting Assistant
+
+    ## get:actors - success
+    def test_casting_assistant_get_list_of_actors(self):
+        res = self.client().get(
+            '/actors', 
+            headers=self.token_auth['casting_assistant_auth'])
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['actors']))
+
+    ## post:actors - forbidden
+    def test_403_forbidden_casting_assistant_create_new_actor(self):
+        res = self.client().post(
+            '/actors',
+            json=self.actor_test, 
+            headers=self.token_auth['casting_assistant_auth'])
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Forbidden')
+
+    # Casting Director
+
+    ## patch:actors - success
+    def test_casting_director_update_actor_information(self):
+        res = self.client().patch(
+            '/actors/3',
+            json=self.actor_test, 
+            headers=self.token_auth['casting_director_auth'])
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['actor'])
+        self.assertEqual(data['actor']['name'], self.actor_test['name'])
+
+    ## post:movies - forbidden
+    def test_403_forbidden_casting_director_create_new_movie(self):
+        res = self.client().post(
+            '/movies',
+            headers=self.token_auth['casting_director_auth'])
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Forbidden')
+
+    # Executive Producer
+
+    ## post:movies - success
+    def test_executive_producer_create_new_movie(self):
+        res = self.client().post(
+            '/movies',
+            json=self.movie_test, 
+            headers=self.token_auth['executive_producer_auth'])
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created_id'])
+
+    ## delete:movies - success
+    def test_executive_producer_delete_movie(self):
+        res = self.client().delete(
+            '/movies/3',
+            headers=self.token_auth['executive_producer_auth'])
+        data = json.loads(res.data.decode('utf-8'))
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['deleted_id'])
 
 
 # Make the tests conveniently executable
