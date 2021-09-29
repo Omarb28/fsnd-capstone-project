967d58ab4ff747b46f836366eeb3c4e87084fe86
Omarb28 <omarb28@gmail.com>
2021-09-29 15:04:52 +0300

    Implement error handling

--- src/app.py
+++ src/app.py
@@ -4,7 +4,9 @@ from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from sqlalchemy import exc
+
 from .database.models import setup_db, Actor, Movie
+from .auth.auth import requires_auth, AuthError
 
 
 def create_app(test_config=None):
@@ -14,12 +16,14 @@ def create_app(test_config=None):
     db = setup_db(app)
     migrate = Migrate(app, db)
 
+
     '''
     Actor Endpoints
     '''
     # Retrive Actor List
     @app.route('/actors', methods=['GET'])
-    def retrieve_actors_list():
+    @requires_auth('get:actors')
+    def retrieve_actors_list(jwt):
         try:
             actors = Actor.query.all()
             formatted_actors = [actor.format() for actor in actors]
@@ -119,11 +123,13 @@ def create_app(test_config=None):
         except exc.SQLAlchemyError:
             abort(400)
 
+
     '''
     Movie Endpoints
     '''
     # Retrieve Movie List
     @app.route('/movies')
+    @requires_auth('get:movies')
     def retrieve_movies_list():
         try:
             movies = Movie.query.all()
@@ -220,6 +226,97 @@ def create_app(test_config=None):
         except exc.SQLAlchemyError:
             abort(400)
 
+
+    '''
+    Error Handling
+    '''
+    # Bad Request
+    @app.errorhandler(400)
+    def bad_request_error(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad Request',
+            'description': error.description
+        }), 400
+
+    # Not Found
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Not Found',
+            'description': error.description
+        }), 404
+
+    # Method Not Allowed
+    @app.errorhandler(405)
+    def method_not_allowed_error(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'Method Not Allowed',
+            'description': error.description
+        }), 405
+
+    # Conflict
+    @app.errorhandler(409)
+    def conflict_error(error):
+        return jsonify({
+            'success': False,
+            'error': 409,
+            'message': 'Conflict',
+            'description': error.description
+        }), 409
+
+    # Unprocessable Entity
+    @app.errorhandler(422)
+    def unprocessable_entity_error(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable Entity",
+            'description': error.description
+        }), 422
+    
+    # Internal Server Error
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'Internal Server Error',
+            'description': error.description
+        }), 500
+    
+    # Authentication and Authorization Errors
+    @app.errorhandler(AuthError)
+    def auth_error_handler(error):
+        status_code = error.status_code
+        error_code = error.error['code']
+        error_description = error.error['description']
+
+        messages = {
+            400: "Bad Request",
+            401: "Unauthorized",
+            403: "Forbidden",
+            422: "Unprocessable Entity"
+        }
+        status_code_message = messages.get(
+            status_code,
+            "Status code message could not be found"
+        )
+
+        return jsonify({
+            "success": False,
+            "error": status_code,
+            "message": status_code_message,
+            "description": error_description,
+            "code": error_code
+        }), status_code
+
+
     return app
 
 
@@ -227,3 +324,5 @@ APP = create_app()
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
+
+
